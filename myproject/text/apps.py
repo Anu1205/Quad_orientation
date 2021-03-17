ce2cd2bf14cf644dae7e855d6576907415dbbc79
Anu1205 <69347894+Anu1205@users.noreply.github.com>
2021-03-17 15:14:52 +0530

    Delete myproject directory

--- myproject/text/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-class textConfig(AppConfig):
-    name = 'text'
-    def contentextraction(self,content):
-        name=content.lower()
-        return {"con":name}
--- myproject/text/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patterns
-# from text import views
-#
-# urlpatterns = [
-#     path('numbers/', views.numbers),
-# ]

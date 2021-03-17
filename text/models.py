8696e28a32712c713c60f3d1488b710678e459bd
Anu1205 <69347894+Anu1205@users.noreply.github.com>
2021-03-17 10:43:24 +0530

    Add files via upload

---
+++ text/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class content(models.Model):
+    info=models.TextField()
+class phnum(models.Model):
+    number=models.CharField(max_length=19)
+    def __int__():
+    	return self.number
